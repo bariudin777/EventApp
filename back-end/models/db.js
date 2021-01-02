d957bc462a3819671d38d0307ab92a0e3a8bb41a
daniel bariudin <daniel.bariudin@live.biu.ac.il>
2021-01-02 19:13:02 +0200

    Form looks like ****, next=> google api and the modal build

--- back-end/models/db.js
+++ back-end/models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 //mongoose
-mongoose.connect(process.env.MONGODB_URI, (err) => {
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true,useUnifiedTopology: true,useFindAndModify:false,useCreateIndex: true}, (err) => {
     if (!err) { console.log('MongoDB connection succeeded.'); }
     else { console.log('Error in MongoDB connection : ' + JSON.stringify(err, undefined, 2)); }
 });
