455806c2a1a95f8d30afd506f20004b4708f010b
daniel bariudin <daniel.bariudin@live.biu.ac.il>
2021-06-06 22:12:40 +0300

    Created sialog for recommendations

--- front-end/src/app/event/new-event/form-modal/modal-recommendations/modal-recommendations.component.ts
+++ front-end/src/app/event/new-event/form-modal/modal-recommendations/modal-recommendations.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Inject } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { InfoDialogComponent} from 'src/app/dialog-manager/info-dialog/info-dialog.component';
+
 
 @Component({
   selector: 'app-modal-recommendations',
@@ -6,16 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modal-recommendations.component.css']
 })
 export class ModalRecommendationsComponent implements OnInit {
+  public modal: any
+  public dialog:any
 
   constructor() { }
 
   ngOnInit(): void {
-
+    this.dialog = document.getElementById("myModal")
+    
   }
 
   displayForm(): void{
-  alert("display")
-}
+    this.dialog.className += " is-active"
+    const other_modals = document.getElementById("select-form-btn")
+    if (other_modals != null) {
+      other_modals.className = "modal"
+    }
+
+  }
 
 
 }
