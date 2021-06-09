48cd969e9328f1f967f75994140c887907b74cf8
daniel bariudin <daniel.bariudin@live.biu.ac.il>
2021-06-09 20:27:34 +0300

    start on dialog work

--- front-end/src/app/dialog-manager/info-dialog/info-dialog.component.ts
+++ front-end/src/app/dialog-manager/info-dialog/info-dialog.component.ts
@@ -6,15 +6,30 @@ import { Component, OnInit,EventEmitter,Output,Input } from '@angular/core';
   styleUrls: ['./info-dialog.component.css']
 })
 export class InfoDialogComponent implements OnInit {
-  private dialog:any
+  private dialog: any
+  public description: string | undefined;
+  public users_recommendations: string|undefined;
   constructor() { }
 
   ngOnInit(): void {
     this.dialog = document.getElementById("info-modal")
+    //get the recommendation from api
+    this.description = `Trip to Eilat: 
+    Transport to destination: car \n
+    Car rental: the best price you get is 400nis.\n
+    Hotels: Hilton\n
+    Dates: 19.2.2021- 21.2.2021\n
+    Price: 1000 nis per person(car not included)`
+    this.users_recommendations = "Daniel: Was great!"
+    //display dynamically
+
+    
   }
   cancel() {
     this.dialog.className= "modal"
   }
   displayForm(): void{
+    //download form with : https://stackoverflow.com/questions/61203930/how-to-export-data-table-to-pdf-using-angular-material
+    
   }
 }
