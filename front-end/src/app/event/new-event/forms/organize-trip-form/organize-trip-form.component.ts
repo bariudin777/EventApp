bd9eb2025ee109b867864cc7706001cfad46c8b6
daniel bariudin <daniel.bariudin@live.biu.ac.il>
2021-06-18 19:39:44 +0300

    working on forms

--- front-end/src/app/event/new-event/forms/organize-trip-form/organize-trip-form.component.ts
+++ front-end/src/app/event/new-event/forms/organize-trip-form/organize-trip-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-organize-trip-form',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./organize-trip-form.component.css']
 })
 export class OrganizeTripFormComponent implements OnInit {
-
+  @Input() modelGroupName: string | undefined;
   constructor() { }
 
   ngOnInit(): void {
